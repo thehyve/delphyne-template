5b4152a535e17c3784a733d8a0eee12e5bb5af30
Alessia Peviani <alessia@thehyve.nl>
2021-01-11 14:22:40 +0100

    Cleanup wrapper

--- src/main/python/wrapper.py
+++ src/main/python/wrapper.py
@@ -13,15 +13,11 @@
 # GNU General Public License for more details.
 
 import logging
-from pathlib import Path
 
 from delphyne import Wrapper as BaseWrapper
 from delphyne.config.models import MainConfig
 
 from src.main.python.transformation import *
-from src.main.python.util import VariableConceptMapper # TODO: add to package?
-from src.main.python.util import OntologyConceptMapper # TODO: add to package?
-from src.main.python.util import RegimenExposureMapper # TODO: add to package?
 from src.main.python import cdm
 
 
@@ -34,23 +30,13 @@ class Wrapper(BaseWrapper):
     def __init__(self, config: MainConfig):
         super().__init__(config, cdm)
 
-        # Load config settings
-        self.path_mapping_tables = Path('./resources/mapping_tables')
-        self.path_sql_transformations = Path('./src/main/sql')
-        # Load data to objects
-        # self.variable_concept_mapper = VariableConceptMapper(self.path_mapping_tables)
-        # self.ontology_concept_mapper = OntologyConceptMapper(self.path_mapping_tables)
-        # self.regimen_exposure_mapper = RegimenExposureMapper(self.path_mapping_tables)
-        # NOTE: replace the following with project-specific source table names!
-        self.sample_source_table = None
-
     def transform(self):
         # NOTE: replace the following with project-specific transformations from python/transformations/ or sql/ folder!
         # make sure execution follows order of table dependencies (see cdm model)
         self.execute_transformation(dm_to_person)
         self.execute_transformation(sample_source_table_to_person)
         self.execute_transformation(sample_source_table_to_condition_occurrence)
-        # self.execute_sql_file(self.path_sql_transformations / 'sample_script.sql')
+        # self.execute_sql_file('sample_script.sql')
 
     def run(self):
         # Prepare source
