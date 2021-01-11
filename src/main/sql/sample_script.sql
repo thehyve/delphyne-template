dc685d0378b0223239b716386dfdf274d456f162
Alessia Peviani <alessia@thehyve.nl>
2021-01-11 14:33:27 +0100

    Refactor sample SQL script

--- src/main/sql/sample_script.sql
+++ src/main/sql/sample_script.sql
@@ -4,9 +4,7 @@ INSERT INTO cdm.observation_period
 (
    person_id,
    observation_period_start_date,
-   observation_period_start_datetime,
    observation_period_end_date,
-   observation_period_end_datetime,
    period_type_concept_id
 )
 with events as (
@@ -14,14 +12,14 @@ with events as (
         'Visit' AS domain_name, person_id, visit_start_date AS start_date, visit_end_date AS end_date,
         visit_concept_id AS concept_id, visit_type_concept_id as type_concept_id, visit_source_value AS source_value,
         visit_occurrence_id
-    FROM visit_occurrence
+    FROM cdm.visit_occurrence
 
     UNION
     SELECT
         'Condition' AS domain_name, person_id, condition_start_date AS start_date, condition_end_date AS end_date,
         condition_concept_id AS concept_id, condition_type_concept_id as type_concept_id, condition_source_value AS source_value,
         visit_occurrence_id
-    FROM condition_occurrence
+    FROM cdm.condition_occurrence
 
     UNION
 
@@ -29,7 +27,7 @@ with events as (
         'Drug' AS domain_name, person_id, drug_exposure_start_date AS start_date, drug_exposure_end_date AS end_date,
         drug_concept_id AS concept_id, drug_type_concept_id as type_concept_id, drug_source_value AS source_value,
         visit_occurrence_id
-    FROM drug_exposure
+    FROM cdm.drug_exposure
 
     UNION
 
@@ -37,7 +35,7 @@ with events as (
         'Procedure' AS domain_name, person_id, procedure_date AS start_date, NULL AS end_date,
         procedure_concept_id AS concept_id, procedure_type_concept_id as type_concept_id, procedure_source_value AS source_value,
         visit_occurrence_id
-    FROM procedure_occurrence
+    FROM cdm.procedure_occurrence
 
     UNION
 
@@ -45,7 +43,7 @@ with events as (
         'Observation' AS domain_name, person_id, observation_date AS start_date, NULL AS end_date,
         observation_concept_id AS concept_id, observation_type_concept_id as type_concept_id, observation_source_value AS source_value,
         visit_occurrence_id
-    FROM observation
+    FROM cdm.observation
 
     UNION
 
@@ -53,25 +51,24 @@ with events as (
         'Measurement' AS domain_name, person_id, measurement_date AS start_date, NULL AS end_date,
         measurement_concept_id AS concept_id, measurement_type_concept_id as type_concept_id, measurement_source_value AS source_value,
         visit_occurrence_id
-    FROM measurement
+    FROM cdm.measurement
 
-    UNION
-
-    SELECT
-        'Death' AS domain_name, person_id, death_date AS start_date, NULL AS end_date,
-        cause_concept_id AS concept_id, death_type_concept_id as type_concept_id, cause_source_value AS source_value,
-        NULL as visit_occurrence_id
-    FROM death
+-- uncomment the following if using CDM v5.3.1
+--     UNION
+--
+--     SELECT
+--         'Death' AS domain_name, person_id, death_date AS start_date, NULL AS end_date,
+--         cause_concept_id AS concept_id, death_type_concept_id as type_concept_id, cause_source_value AS source_value,
+--         NULL as visit_occurrence_id
+--     FROM cdm.death
 )
 select
     person.person_id,
     min(start_date) AS observation_start_date,
-    min(start_date) AS observation_start_datetime,
     greatest(max(start_date), max(end_date)) AS observation_end_date,
-    greatest(max(start_date), max(end_date)) AS observation_end_datetime,
     44814724 as observation_type_concept_id  -- Period covering healthcare encounters
-from person
-left join events on person.person_id = events.person_id
+from cdm.person
+left join events on cdm.person.person_id = events.person_id
 where start_date > '1970-01-01'
-group by person.person_id
+group by cdm.person.person_id
 ;
