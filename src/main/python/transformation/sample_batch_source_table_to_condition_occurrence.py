d74390efdd8a374337e390136b4234e9f49715f8
Spayralbe <stefan@thehyve.nl>
2021-02-23 15:12:51 +0100

    Remove person_id insert from batch query

--- src/main/python/transformation/sample_batch_source_table_to_condition_occurrence.py
+++ src/main/python/transformation/sample_batch_source_table_to_condition_occurrence.py
@@ -2,15 +2,16 @@ from __future__ import annotations
 
 from typing import Generator, TYPE_CHECKING
 
-# sample functions, remove imports if not used
-from src.main.python.util import create_person_id_from_subject_id
+# sample function, remove import if not used
 from src.main.python.util import get_datetime
 
 if TYPE_CHECKING:
     from src.main.python.wrapper import Wrapper
 
 
-def sample_batch_source_table_to_condition_occurrence(wrapper: Wrapper) -> Generator[Wrapper.cdm.ConditionOccurrence]:
+def sample_batch_source_table_to_condition_occurrence(
+        wrapper: Wrapper
+) -> Generator[Wrapper.cdm.ConditionOccurrence]:
     # Memory efficient inserting.
     # 1. iterate through source data with a generator
     # 2. return transformed records as a generator
@@ -21,7 +22,6 @@ def sample_batch_source_table_to_condition_occurrence(wrapper: Wrapper) -> Gener
 
     for row in data:
         yield wrapper.cdm.ConditionOccurrence(
-            person_id=create_person_id_from_subject_id(row['subject_id']),
             condition_start_datetime=get_datetime(),
             condition_concept_id=0,
             condition_source_concept_id=0,
